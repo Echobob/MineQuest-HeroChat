b692473ed8516c7af2061fcbbc3a687907542a27
Echobob <echobob@gmail.com>
2012-08-07 13:40:26 -0400

    Changes to pom and License

--- src/main/java/com/theminequest/MQHeroChat/MQHeroChat.java
+++ src/main/java/com/theminequest/MQHeroChat/MQHeroChat.java
@@ -8,6 +8,7 @@ import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import com.theminequest.MineQuest.API.Managers;
+import com.dthielke.herochat.*;
 
 public class MQHeroChat {
 
